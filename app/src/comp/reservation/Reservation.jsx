f390e4f28e351ca9d894b588b6f366319dd7f412
Oliver Szabo <oliverszbo@gmail.com>
2021-08-31 11:33:26 +0200

    contact info

--- app/src/comp/reservation/Reservation.jsx
+++ app/src/comp/reservation/Reservation.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "../../css/reservation/reservation.scss";
+import phone from "../../img/smartphone.png";
+import facebook from "../../img/facebook.png";
+import instagram from "../../img/instagram.png";
+import email from "../../img/email.png";
 
 const Reservation = () => {
   return (
@@ -7,7 +11,51 @@ const Reservation = () => {
       <div className="container">
         <h1>Időpontfoglalás</h1>
 
-        <p>A xyz telefonszámon.</p>
+        <p>
+          Keress minket telefonszámunkon, ahol rendelkezésedre állunk a
+          nyitvatartási időnk keretein belül, valamint megtalálsz minket
+          valamennyi közösségi médián, vagy e-mailen keresztül.
+        </p>
+
+        <div className="kapcsolat">
+          <div className="icons">
+            <div className="icon-img">
+              <img src={phone} alt="phone" />
+            </div>
+            <div className="p">+ 36 70 454 4980</div>
+          </div>
+
+          <div className="icons">
+            <a
+              href="https://www.facebook.com/InfraslimXHillside"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="icon-img">
+                <img src={facebook} alt="facebook" />
+              </div>
+            </a>
+          </div>
+
+          <div className="icons">
+            <a
+              href="https://www.instagram.com/infraslimx_hillside/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="icon-img">
+                <img src={instagram} alt="facebook" />
+              </div>
+            </a>
+          </div>
+
+          <div className="icons">
+            <div className="icon-img">
+              <img src={email} alt="email" />
+            </div>
+            <div className="p">infraslimxhillside@gmail.com</div>
+          </div>
+        </div>
       </div>
     </div>
   );
